3a9882b05992aebea723cc14472fe6a0269464d5
soundarya-tech <soundaryagji@gmail.com>
2023-12-08 15:25:59 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("animationButton").addEventListener("click", () => {
+    // console.log("check");
+    var square = document.getElementById("animationSquare");
+    square.classList.remove("animate")
+    square.offsetWidth
+    square.classList.add("animate")
+})
